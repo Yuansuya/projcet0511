50a1aaa058da67763b7b497a864d413d225d9859
yuan <kevincolin933@gmail.com>
2018-05-18 10:12:08 +0800

    C:\Users\user\Desktop\223\projcet0511

---
+++ simof.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+		int init,endpoint=10,summary;
+	// »¼¥[½m²ß 
+	for(init=0,summary=0;init<endpoint;init++)
+		summary=summary+init;
+	return 0;
+}
